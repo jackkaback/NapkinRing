cdf42f85c4b4a158def85947baf3a3b1b3981955
jack <jackkaback@gmail.com>
2018-11-20 11:46:43 -0500

    fixed issue where some digits wouldn't show

--- main.cpp
+++ main.cpp
@@ -8,12 +8,16 @@ int main() {
 	while(1) {
 		double height = 0.0;
 		double volume = 0.0;
-		std::cout << "Enter a Height for the napkin ring: ";
+		std::cout << "Enter a Height for the napkin ring (-1 to exit): ";
 		std::cin >> height;
+
+		if (height <= 0){
+			break;
+		}
 
 		volume = std::pow(height, 3.0) * PI / 6.0;
 
-		std::cout << std::endl << "Volume: " << volume;
+		printf("Volume: %10lf\n", volume);
 	}
 
 	return 0;
