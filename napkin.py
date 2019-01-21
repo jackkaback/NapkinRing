8335cbde93d11534636706d71906ce939d52c51d
jack <jackkaback@gmail.com>
2019-01-21 15:51:12 -0500

    support py2 and py3

--- napkin.py
+++ napkin.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python2.7
-
+from __future__ import print_function
+from six.moves import *
 import math
 
 while True:	
-	height = input("Enter the height of the napkin ring: ")
-	volume = 0.0
-	
+	height = float(input("Enter the height of the napkin ring: "))
 	volume = height**3 * math.pi / 6
 	
-	print "Volume is " + str(volume)
-	
-	
+	print("Volume is " + str(volume))
