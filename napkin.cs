3004d7da2ce46b73ac3554885fcd40d029cf0f9f
jack <jackkaback@gmail.com>
2019-08-05 12:33:42 -0400

    adding C# support

---
+++ napkin.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace napkin
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world");
+
+			double height = 0.0;
+			double volume = 0.0;
+
+			while(true)
+			{
+				Console.WriteLine("Enter the height of the napkin ring");
+				string temp = Console.ReadLine();
+
+				if (double.TryParse(temp, out height))
+				{
+					volume = calcVolume(height);
+
+					Console.WriteLine(volume);
+				}
+			}
+		}
+
+		public static double calcVolume(double height)
+		{
+			double retVal = Math.Pow(height, 3) * Math.PI * (1.0 / 6.0);
+
+			return retVal;
+		}
+	}
+}
